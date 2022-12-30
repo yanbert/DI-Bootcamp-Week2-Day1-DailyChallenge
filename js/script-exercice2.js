a184d5c67b5cc7cda8c68364e5da2c53f81dfb5a
yanbert <yanbert@gmail.com>
2022-12-30 00:49:34 +0000

    Merci de voir mes commentaires dans le code

--- js/script-exercice2.js
+++ js/script-exercice2.js
@@ -4,4 +4,10 @@ const moreFruits = ["Banana", ["Apples", ["Oranges"], "Blueberries"]];
 /*let access1=moreFruits[1];
 let access2=access1[1];
 let access3=access2[0];*/
+
+/****************************************************
+Correcteur: Yannick GOUGUIA
+
+Merci de revoir cet exercice
+****************************************************/
 console.log(moreFruits[1][1][0])
